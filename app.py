e7e88f779b3d09975cf51d5d6daa5834858b80ef
ventasistemas52012-source <ventasistemas52012@gmail.com>
2026-03-07 23:50:49 -0500

    app.py

--- app.py
+++ app.py
@@ -1076,4 +1076,9 @@ def exportar_todos():
 
 
 
-if __name__ == "__main__": app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
